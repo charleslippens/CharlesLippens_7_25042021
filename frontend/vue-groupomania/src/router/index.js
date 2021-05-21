9a11c195b7bdc9e9ac64eb804ab4d6f40a723d2a
CL <cl@gmail.com>
2021-05-21 12:58:49 +0200

    modifications navigation guards

--- frontend/vue-groupomania/src/router/index.js
+++ frontend/vue-groupomania/src/router/index.js
@@ -1,50 +1,69 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import auth from '../middleware/auth'
-import VueRouteMiddleware from 'vue-route-middleware'
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import auth from "../middleware/auth";
+const isAdmin = localStorage.getItem("isAdmin");
+import VueRouteMiddleware from "vue-route-middleware";
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/signup',
-    name: 'Signup',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Signup.vue')
-  },
-  {
-    path: '/post',
-    name: 'Post',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Post.vue'),
-    meta: {
-      middleware: auth
-    }
-  },
-  {
-    path: '/profile',
-    name: 'Profile',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Profile.vue'),
-    meta: {
-      middleware: auth
-    }
-  },
-  {
-		path: "/users",
-		name: "Users",
-		component: () => import(/* webpackChunkName: "about" */ "../views/Users.vue"),
+	{
+		path: "/",
+		name: "Home",
+		component: Home,
+		beforeEnter: (to, from, next) => {
+			if (!localStorage.getItem("token")) {
+				return next();
+			}
+			return next("/post");
+		},
+	},
+	{
+		path: "/signup",
+		name: "Signup",
+		component: () => import(/* webpackChunkName: "about" */ "../views/Signup.vue"),
+		beforeEnter: (to, from, next) => {
+			if (!localStorage.getItem("token")) {
+				return next();
+			}
+			return next("/post");
+		},
+	},
+	{
+		path: "/post",
+		name: "Post",
+		component: () => import(/* webpackChunkName: "about" */ "../views/Post.vue"),
 		meta: {
 			middleware: auth,
 		},
-	}
-]
+	},
+	{
+		path: "/profile",
+		name: "Profile",
+		component: () => import(/* webpackChunkName: "about" */ "../views/Profile.vue"),
+		meta: {
+			middleware: auth,
+		},
+	},
+	{
+		path: "/users",
+		name: "Users",
+		component: () => import(/* webpackChunkName: "about" */ "../views/Users.vue"),
+		beforeEnter: (to, from, next) => {
+			if (isAdmin == "true") {
+				return next();
+			}
+			return next("/post");
+		},
+		meta: {
+			middleware: auth,
+		},
+	},
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
+	history: createWebHashHistory(),
+	routes,
+});
 
-router.beforeEach(VueRouteMiddleware())
+router.beforeEach(VueRouteMiddleware());
 
-export default router
+export default router;
